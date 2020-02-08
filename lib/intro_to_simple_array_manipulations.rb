385141a90bb30d123fc89c10b5cf417b2b44395e
Joshua Gauthreaux <joshuagauthreaux@gmail.com>
2020-02-08 18:13:52 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,3 @@
-
-    array = []
-    array.push("string")
+colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+    next_color = "violet"
+    colors_in_the_rainbow.push(next_color)
