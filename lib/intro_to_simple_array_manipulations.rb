d6e62fc74d720babd21cb57227f9713908ac1158
Joshua Gauthreaux <joshuagauthreaux@gmail.com>
2020-02-08 19:51:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,13 +3,9 @@ def using_push (array, string)
 end
 
 def using_unshift (array, string)
-    bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
-    new_neighborhood = "Staten Island"
-    # bouroughs_in_nyc.unshift(new_neighborhood)
     array.unshift(string)
 end
   
 def using_pop (array)
-    continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
     array.pop
 end
